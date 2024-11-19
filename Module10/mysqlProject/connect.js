6f4d0cbf0174b49e24c10a5678085cb31d1fd806
Sujan-Acharya <acharyasujan2060@gmail.com>
2024-11-18 19:12:25 -0500

    dd

---
+++ Module10/mysqlProject/connect.js
@@ -0,0 +1,102 @@
+const mysql = require("mysql2");
+const fs = require("fs");
+const operations = require("./lib/operations");
+
+const mysqlConnect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "$Sumit2015810",
+});
+
+mysqlConnect.connect((err) => {
+  if (err) {
+    console.log(" Error connecting to database");
+  } else {
+    console.log("Successfully connected to db");
+  }
+});
+
+//Create Database if not exists.
+
+let create_db_sql = "CREATE DATABASE if not exists StudTrack;";
+
+mysqlConnect.query(create_db_sql, (err) => {
+  if (err) {
+    console.log("Unable to create STudTrack database = " + err.code);
+  }
+});
+
+let create_student_tb =
+  " CREATE table if not exists Students (S_CODE VARCHAR(10) NOT NULL PRIMARY KEY, S_NAME VARCHAR(25) NOT NULL, \
+ S_YEAR ENUM ('1st', '2nd', '3rd', '4th'))";
+
+let create_course_tb =
+  " CREATE table if not exists course (C_CODE VARCHAR(10) NOT NULL PRIMARY KEY, c_NAME VARCHAR(255) NOT NULL, \
+ C_BOOK VARCHAR(255) NOT NULL, S_YEAR ENUM ('1st', '2nd', '3rd', '4th'))";
+
+let create_student_course_tb =
+  " CREATE table if not exists studentcourse (S_CODE VARCHAR(10) NOT NULL, c_CODE VARCHAR(10) NOT NULL, \
+ GRADE VARCHAR(2) NOT NULL, CONSTRAINT FOREIGN KEY (S_CODE) REFERENCES STUDENTS(S_CODE), CONSTRAINT FOREIGN KEY (C_CODE) REFERENCES COURSE(C_CODE),\
+ PRIMARY KEY(S_CODE, C_CODE) )";
+
+mysqlConnect.query("USE StudTrack", (err) => {
+  if (err) throw err;
+});
+
+mysqlConnect.query(create_student_tb, (err) => {
+  if (err) throw err;
+  console.log("Created Students Table");
+});
+
+mysqlConnect.query(create_course_tb, (err) => {
+  if (err) throw err;
+  console.log("Created Course Table");
+});
+
+mysqlConnect.query(create_student_course_tb, (err) => {
+  if (err) throw err;
+  console.log("Created StudentCourse Table");
+});
+
+let op = process.argv[2];
+let table = process.argv[3];
+
+switch (op.toUpperCase()) {
+  case "ADD":
+    let buffer = fs.readFileSync("./s_obj.json");
+    let obj = JSON.parse(buffer);
+    console.log("Obj = ", obj);
+    operations.add(mysqlConnect, table, obj, (results) => {
+      console.log(results);
+    });
+    break;
+  case "DELETE":
+    let buffer_del = fs.readFileSync("./s_obj.json");
+    let obj_del = JSON.parse(buffer);
+    console.log("Obj = ", obj);
+    operations.delete(mysqlConnect, table, obj_del, (results));
+        console.log(results); });
+    break;
+  case "DISPLAY":
+    operations.display(mysqlConnect, table, (results) => {
+      console.log("The results are\n");
+      console.log(results);
+    });
+    break;
+  case "UPDATE":
+    let buffer_upd = fs.readFileSync("./s_obj.json");
+    let obj_upd = JSON.parse(buffer_upd);
+    console.log("Obj = ", obj_upd);
+    operations.update(mysqlConnect, table, obj_upd, (results) => {
+      console.log(results);
+    });
+    break;
+}
+
+mysqlConnect.end((err) => {
+  if (err) {
+    console.log("Error while closing database connection");
+  } else {
+    console.log(" Database connection closed ...");
+  }
+});
---
+++ Module10/mysqlProject/lib/operations.js
@@ -0,0 +1,94 @@
+const mysql = require("mysql2");
+
+exports.add = function (conn, table, obj, cb) {
+  let course_sql =
+    "insert into StudTrack.Course (C_CODE, C_NAME, C_BOOK, S_YEAR) VALUES (?, ?, ?, ?)";
+  let student_sql =
+    " insert into StudTrack.Students (S_CODE, S_NAME, S_year) values (?, ?, ?)";
+  let studcourse_sql =
+    " insert into StudTrack.studentcourse (S_CODE, C_CODE, GRADE) values (?, ?, ?)";
+  let sql = "";
+  let values = [];
+
+  if (table == "Students") {
+    sql = student_sql;
+    values = [obj.S_CODE, obj.S_NAME, obj.S_YEAR];
+  } else if (table == "Course") {
+    sql = course_sql;
+    values = [obj.C_CODE, obj.C_NAME, obj.C_BOOK, obj.S_YEAR];
+  } else {
+    sql = studcourse_sql;
+    values = [obj.S_CODE, obj.C_CODE, obj.GRADE];
+  }
+
+  conn.query(sql, values, (err, results) => {
+    if (err) throw err;
+    cb(results);
+  });
+};
+
+exports.display = function (conn, table, cb) {
+  let sql = "";
+  if (table == "Students") {
+    sql = " SELECT * from students";
+  } else if (table == "Course") {
+    sql = " SELECT * FROM Course";
+  } else {
+    sql = "SELECT * FROM StudentCourse";
+  }
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    cb(result);
+  });
+};
+
+exports.update = function (conn, table, obj, cb) {
+  let course_sql = " update StudTrack.Course set C_YEAR = ? where C_CODE = ?";
+  let student_sql =
+    " UPDATE StudTrack.Students set S_YEAR = ? where S_CODE = ?";
+  let studcourse_sql =
+    " update StudTrack.StudentCourse set GRADE = ? where S_CODE = ? and C_CODE = ? ";
+  let sql = "";
+  let values = [];
+
+  if (table == "Students") {
+    sql = student_sql;
+    values = [obj.S_YEAR, obj.S_CODE];
+  } else if (table == "Course") {
+    sql = course_sql;
+    values = [obj.C_YEAR, obj.C_CODE];
+  } else {
+    sql = studcourse_sql;
+    values = [obj.GRADE, obj.S_CODE, obj.C_CODE];
+  }
+
+  conn.query(sql, values, (err, results) => {
+    if (err) throw err;
+    cb(results);
+  });
+};
+
+exports.delete = function (db, table, obj, cb) {
+  let student_del = "delete from StudTrack.Students WHERE S_CODE = ?";
+  let course_del = "delete from StudTrack.Course WHERE C_CODE = ?";
+  let student_course_del =
+    "delete from StudTrack.StudentCourse WHERE S_CODE = ? AND C_CODE = ?";
+  let sql = "";
+  let values = [];
+
+  if (table == "Students") {
+    sql = student_del;
+    values = [obj.S_CODE];
+  } else if (table == "Course") {
+    sql = course_del;
+    values = [obj.C_CODE];
+  } else {
+    sql = student_course_del;
+    values = [obj.S_CODE, obj.C_CODE];
+  }
+
+  db.query(sql, values, (err, results) => {
+    if (err) throw err;
+    cb(results);
+  });
+};
